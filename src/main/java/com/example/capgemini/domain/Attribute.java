61d49bc5bbf05e494ec7f38e1040adfb76a58648
admin <lavr666.ru@mail.ru>
2019-12-25 16:06:48 +0300

    add index geometryIndex

--- src/main/java/com/example/capgemini/domain/Attribute.java
+++ src/main/java/com/example/capgemini/domain/Attribute.java
@@ -5,11 +5,14 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Index;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
 
 @Entity
-//@Table(name = "attribute", indexes = {@Index(name = "geometryIndex", columnList = "geometry")})
+@Table(name = "attribute", indexes = {@Index(name = "geometryIndex", columnList = "geometry")})
 public class Attribute {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
