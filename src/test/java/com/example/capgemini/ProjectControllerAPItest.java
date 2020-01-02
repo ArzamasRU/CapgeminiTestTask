f6ed462f9ddf16188ae1dbf5506266247f6f238f
admin <lavr666.ru@mail.ru>
2020-01-02 19:13:40 +0300

    add api test

--- src/test/java/com/example/capgemini/ProjectControllerAPItest.java
+++ src/test/java/com/example/capgemini/ProjectControllerAPItest.java
@@ -1,8 +1,10 @@
 package com.example.capgemini;
 
 import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.Matchers.iterableWithSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.Test;
@@ -13,6 +15,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.example.capgemini.repos.ProjectRepo;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -20,10 +24,20 @@ public class ProjectControllerAPItest {
 	@Autowired
 	private MockMvc mockMvc;
 
+	@Autowired
+	private ProjectRepo projectRepo;
+
 	@Test
 	public void contextLoads() throws Exception {
 		mockMvc.perform(get("/"))
 				.andExpect(status().isOk())
 				.andExpect(content().string(containsString("This is a test task for capgemini")));
+	}
+
+	@Test
+	public void apiControllerShouldReturnVisits() throws Exception {
+		int projectAmount = projectRepo.findAll().size();
+		mockMvc.perform(get("/api/projects"))
+				.andExpect(jsonPath("$.*.name", iterableWithSize(projectAmount)));
 	}
 }
